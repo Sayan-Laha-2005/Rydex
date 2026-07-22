06095543b41d79287714900233d639bd93bc6c33
Sayan-Laha-2005 <sayanlahaclash@gmail.com>
2026-07-22 12:05:37 +0530

    Update navigation and user hook

--- src/hooks/useGetMe.tsx
+++ src/hooks/useGetMe.tsx
@@ -1,20 +1,28 @@
 'use client'
 
+import { setUserData } from '@/redux/userSlice'
 import axios from 'axios'
 import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 
-function useGetMe(enabled:boolean) {
-    
-  useEffect(()=>{
-    if(!enabled){
-        return
-    }
-    const getMe=async ()=>{
-        const {data}=await axios.get("/api/user/me")
-        console.log(data)
-    }
-    getMe()
-  },[enabled])
+function useGetMe(enabled: boolean) {
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        if (!enabled) {
+            return
+        }
+        const getMe = async () => {
+            try {
+                const { data } = await axios.get("/api/user/me")
+                dispatch(setUserData(data))
+            } catch (error) {
+                console.log(error)
+            }
+
+        }
+        getMe()
+    }, [enabled])
 }
 
 export default useGetMe
